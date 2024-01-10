d306f8972c4c23af4b96fd119558e97ceee38ca8
unknown <danielfm.c222@hotmail.com>
2024-01-10 00:32:14 -0500

    init js

---
+++ main.js
@@ -0,0 +1,43 @@
+//referencias de los inputs:
+const inputCard = document.querySelector('#inputCard');
+const inputDate = document.getElementById('inputDate');
+const inputCVV = document.querySelector('#inputCVV');
+
+//MASCARAS:   SE DEFINEN LOS PATRONES Y LUEGO SE SUSTITUYEN
+const maskNumber = '####-####-####-####';
+const maskDate = '##/##';
+const maskCVV = '###';
+
+//Listas para almacenar los #s, transformarlos y mostrarlos en el input
+let current = '';
+let cardNumber = [];
+let dateNumber = [];
+let cvvNumber = [];
+
+//EventListeners:
+inputCard.addEventListener('keydown', (e) => {
+  if (e.key == 'Tab') {
+    return;
+  }
+  e.preventDefault();
+  handleInput(maskNumber, e.key, cardNumber);
+  inputCard.value = cardNumber.join('')
+});
+
+function handleInput(mask, key, arr){
+  let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; //Arr de teclas permitidas al usuario para usar... en este caso solo los #s
+  if(key == "Backspace" && arr.length > 0){
+    arr.pop();
+    return;
+  };
+  if(numbers.includes(key) && arr.length + 1 <= mask.length){  //se valida si la tecla presionada está dentro de las permitidas (arr numbers) - devuelve true o false
+    if(mask[arr.length] == '-' || mask[arr.length] == '/'){
+        arr.push(mask[arr.length], key);
+    }else{
+      arr.push(key)
+    }
+  }
+}
+
+
+
