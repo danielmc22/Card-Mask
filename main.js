c0bec66f36ee767ee6fe8e51973e6682d0b906fc
unknown <danielfm.c222@hotmail.com>
2024-01-10 23:39:30 -0500

    js finished and styles started

--- main.js
+++ main.js
@@ -14,15 +14,35 @@ let cardNumber = [];
 let dateNumber = [];
 let cvvNumber = [];
 
-//EventListeners:
+//EventListeners y llamado a fx's por cada input:
 inputCard.addEventListener('keydown', (e) => {
   if (e.key == 'Tab') {
     return;
   }
   e.preventDefault();
   handleInput(maskNumber, e.key, cardNumber);
-  inputCard.value = cardNumber.join('')
+  inputCard.value = cardNumber.join('');
 });
+
+inputDate.addEventListener('keydown', (e) => {
+  if(e.key == 'Tab'){
+    return;
+  }
+  e.preventDefault();
+  handleInput(maskDate, e.key, dateNumber);
+  inputDate.value = dateNumber.join('');
+});
+
+inputCVV.addEventListener('keydown', (e) => {
+  if(e.key == 'Tab'){
+    return;
+  }
+  e.preventDefault();
+  handleInput(maskCVV, e.key, cvvNumber);
+  inputCVV.value = cvvNumber.join('');
+});
+
+
 
 function handleInput(mask, key, arr){
   let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; //Arr de teclas permitidas al usuario para usar... en este caso solo los #s
